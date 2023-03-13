9782f483361cf583432679d6b04b2f89d01ed671
jorgeallende <jorgeallende333@gmail.com>
2023-03-13 10:59:54 -0300

    React router instalado e primeiras rotas

--- front/agendamento-sus/src/main.tsx
+++ front/agendamento-sus/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './routes/Login'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/login', element: <Login /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
