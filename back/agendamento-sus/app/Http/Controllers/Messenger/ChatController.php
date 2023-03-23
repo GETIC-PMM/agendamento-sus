d21a53f9789a8536a6a4fe4410101a3ed63fc5c9
GETIC-PMM <ti@prefeiturademossoro.com.br>
2023-03-23 16:51:53 -0300

    chatbot #2 --first flow

--- back/agendamento-sus/app/Http/Controllers/Messenger/ChatController.php
+++ back/agendamento-sus/app/Http/Controllers/Messenger/ChatController.php
@@ -14,14 +14,27 @@ class ChatController extends Controller
     public function handle()
     {
         $botman = app('botman');
-
-        $botman->hears('{message}', function ($botman, $message) {
-
-            if ($message == 'Oi') {
-                $this->askName($botman);
-            } else {
-                $botman->reply("Não posso ajudar");
-            }
+        $botman->hears('Oi', function ($botman) {
+            $botman->reply(
+                'Olá, bem vindo ao Sistema de Agendamento de Consultas online Do ESUS.
+            No momento, você pode agendar consultas com os seguintes profissionais:
+            - Clínico Geral;
+            - Odontólogo.
+            Número de vagas disponíveis podem variar para cada Unidade de Saúde.'
+            );
+            $this->handleInfo($botman);
+            // });
+            // $botman->ask('Oi, qual seu nome?', function (Answer $answer) {
+            //     $name = $answer->getText();
+            //     $this->say('Olá ' . $name . ', como posso ajudar?');
+            //     $this->hears('{message}', function ($botman, $message) {
+            //         if ($message == 'cpf') {
+            //             $this->askInfo($botman);
+            //         } else {
+            //             $botman->reply("Não posso ajudar");
+            //         }
+            //     });
+            // });
         });
 
         $botman->listen();
@@ -30,17 +43,22 @@ class ChatController extends Controller
     /**
      * Place your BotMan logic here.
      */
-    public function askName($botman)
+    public function handleInfo($botman)
     {
-        $botman->ask('Me pergunte sobre alguém, e direi minha opinião:', function (Answer $answer) {
-
-            $name = $answer->getText();
-            if ($name == 'Pedro Avelino') {
-                $this->say('Burguês safado e sem vergonha');
+        $botman->ask('Para começar, digite seu CPF (apenas números):', function (Answer $answer) {
+            $cpf = $answer->getText();
+            if ($cpf == '123456789') {
+                $this->say('Olá Leopoldo, como posso ajudar?');
             } else {
-                $this->say('Anjinho');
-            }
+                $this->invalidInfo($this);
+            };
         });
+    }
+
+    public function invalidInfo($botman)
+    {
+        $botman->say('CPF inválido, tente novamente.');
+        $botman->handleInfo($botman);
     }
 
     public function frame()
