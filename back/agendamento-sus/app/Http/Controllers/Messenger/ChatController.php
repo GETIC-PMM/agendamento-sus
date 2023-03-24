9a9e20de0e5470256842527879943de366c89126
GETIC-PMM <ti@prefeiturademossoro.com.br>
2023-03-24 16:16:43 -0300

    ChatBot #3 --test conversation

--- back/agendamento-sus/app/Http/Controllers/Messenger/ChatController.php
+++ back/agendamento-sus/app/Http/Controllers/Messenger/ChatController.php
@@ -1,10 +1,17 @@
 <?php
 
+
 namespace App\Http\Controllers\Messenger;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use BotMan\BotMan\BotMan;
 use BotMan\BotMan\Messages\Incoming\Answer;
 use Illuminate\Http\Request;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Cache\LaravelCache;
+use BotMan\BotMan\Drivers\DriverManager;
+use InfoConversation;
 
 class ChatController extends Controller
 {
@@ -14,53 +21,13 @@ class ChatController extends Controller
     public function handle()
     {
         $botman = app('botman');
-        $botman->hears('Oi', function ($botman) {
-            $botman->reply(
-                'Olá, bem vindo ao Sistema de Agendamento de Consultas online Do ESUS.
-            No momento, você pode agendar consultas com os seguintes profissionais:
-            - Clínico Geral;
-            - Odontólogo.
-            Número de vagas disponíveis podem variar para cada Unidade de Saúde.'
-            );
-            $this->handleInfo($botman);
-            // });
-            // $botman->ask('Oi, qual seu nome?', function (Answer $answer) {
-            //     $name = $answer->getText();
-            //     $this->say('Olá ' . $name . ', como posso ajudar?');
-            //     $this->hears('{message}', function ($botman, $message) {
-            //         if ($message == 'cpf') {
-            //             $this->askInfo($botman);
-            //         } else {
-            //             $botman->reply("Não posso ajudar");
-            //         }
-            //     });
-            // });
-        });
-
         $botman->listen();
     }
 
-    /**
-     * Place your BotMan logic here.
-     */
-    public function handleInfo($botman)
+    public function startConversation(BotMan $bot)
     {
-        $botman->ask('Para começar, digite seu CPF (apenas números):', function (Answer $answer) {
-            $cpf = $answer->getText();
-            if ($cpf == '123456789') {
-                $this->say('Olá Leopoldo, como posso ajudar?');
-            } else {
-                $this->invalidInfo($this);
-            };
-        });
+        $bot->startConversation(new InfoConversation());
     }
-
-    public function invalidInfo($botman)
-    {
-        $botman->say('CPF inválido, tente novamente.');
-        $botman->handleInfo($botman);
-    }
-
     public function frame()
     {
         return view('chatbot.frame');
---
+++ back/agendamento-sus/app/Http/Controllers/Messenger/InfoConversation.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use BotMan\BotMan\Messages\Conversations\Conversation;
+use BotMan\BotMan\Messages\Incoming\Answer;
+
+class InfoConversation extends Conversation
+{
+    protected $cpf;
+
+    public function askName()
+    {
+        $this->ask('Seu cpf:', function (Answer $answer) {
+            $this->cpf = $answer->getText();
+            $this->say('teste');
+            // $end = $this->checkName($this->cpf);
+            // $this->say($end);
+        });
+    }
+
+    public function checkName($email)
+    {
+        $user = User::where('email', $email)->first();
+        if ($user) {
+            return 'Olá ' . $user->name . ', como posso ajudar?';
+        } else {
+            return 'Usuário não encontrado';
+        }
+    }
+
+    public function run()
+    {
+        $this->askName();
+    }
+}
