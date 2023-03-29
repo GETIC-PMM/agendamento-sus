59115646f3e9e5823ce939c3fb6e4c9ddf14040d
GETIC-PMM <ti@prefeiturademossoro.com.br>
2023-03-29 11:23:34 -0300

    ajuste rotas

--- back/agendamento-sus/routes/api.php
+++ back/agendamento-sus/routes/api.php
@@ -22,11 +22,11 @@ use Illuminate\Support\Facades\Route;
 Route::controller(RegisterController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
-    Route::get('users', 'list');
 });
 
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('users', 'list');
     Route::resource('units', UnitController::class);
     Route::resource('appointment-types', AppointmentTypeController::class);
     Route::resource('patients', PatientController::class);
