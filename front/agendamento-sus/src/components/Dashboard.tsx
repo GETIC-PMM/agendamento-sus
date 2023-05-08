2954d80d015dc2da297091c6ba3820cb3fcc3135
guilermmm <guilherme.r.a.melo@gmail.com>
2023-05-08 11:32:23 -0300

    finalizado dashboard

--- front/agendamento-sus/src/components/Dashboard.tsx
+++ front/agendamento-sus/src/components/Dashboard.tsx
@@ -1,54 +1,19 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import Paper from '@mui/material/Paper';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import {
-  Checkbox,
-  CircularProgress,
-  FormControlLabel,
-  MenuItem,
-  Modal,
-  Select,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
-} from '@mui/material';
-import { cpfFormatter } from '../utils/cpf-formatter';
-import Typography from '@mui/material/Typography';
+import { CircularProgress, MenuItem, Modal, Select } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import * as dayjs from 'dayjs';
-import {
-  Appointment,
-  Unidade,
-  AppointmentType,
-} from '../interfaces/interfaces';
-import { AppointmentForm } from '@devexpress/dx-react-scheduler';
-import { useGetUnits } from '../api/routes/units-api';
+import { useState } from 'react';
 import {
   useMutateCancelAppointment,
   useUnitAppointmentsById,
 } from '../api/routes/appointments-api';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useGetUnits } from '../api/routes/units-api';
+import { Appointment } from '../interfaces/interfaces';
+import { cpfFormatter } from '../utils/cpf-formatter';
 
 const Dashboard = () => {
   const [selectedUnit, setSelectedUnit] = useState('0');
   const [mutateModal, setMutateModal] = useState(false);
   const [mutateRow, setMutateRow] = useState<Appointment>();
-
-  // const [unitAppointments, setUnitAppointments] = useState<Appointment[]>([]);
-  const [unitAppointmentTypes, setUnitAppointmentTypes] = useState<
-    AppointmentType[]
-  >([]);
-  const [cpfToFilter, setCpfToFilter] = useState<string>('');
-  const [appointmentSearch, setAppointmentSearch] = useState('');
-  const [is7DayFilterChecked, setIs7DayFilterChecked] = useState<boolean>(true);
-  const [isAppointmentDataLoading, setIsAppointmentDataLoading] =
-    useState<boolean>(false);
 
   const units = useGetUnits();
 
@@ -176,9 +141,7 @@ const Dashboard = () => {
         <Select
           value={selectedUnit}
           onChange={e => {
-            setIsAppointmentDataLoading(true);
             setSelectedUnit(e.target.value);
-            // getUnitAppointments(e.target.value, is7DayFilterChecked);
           }}
           sx={{ width: '100%' }}
         >
@@ -188,93 +151,6 @@ const Dashboard = () => {
             </MenuItem>
           ))}
         </Select>
-        <div className="mt-4">
-          {/* <Accordion>
-            <AccordionSummary>
-              <Typography>Filtrar por CPF</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div className="flex gap-2">
-                <TextField
-                  value={cpfToFilter}
-                  onChange={e => {
-                    setCpfToFilter(e.target.value);
-                  }}
-                  label="CPF"
-                  sx={{ width: '100%' }}
-                />
-                <button
-                  className="bg-green-500 py-2 px-6 rounded text-white"
-                  onClick={() => {
-                    setIsAppointmentDataLoading(true);
-                    // getUnitAppointmentsByCPF(
-                    //   selectedUnit,
-                    //   cpfToFilter,
-                    //   is7DayFilterChecked,
-                    // );
-                  }}
-                >
-                  Filtrar
-                </button>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary>
-              <Typography>Filtrar por Atendimento</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <div className="flex gap-2">
-                <TextField
-                  value={appointmentSearch}
-                  onChange={e => {
-                    setAppointmentSearch(e.target.value);
-                  }}
-                  label="Atendimento"
-                  sx={{ width: '100%' }}
-                />
-                <button
-                  className="bg-green-500 py-2 px-6 rounded text-white"
-                  onClick={() => {
-                    // setIsAppointmentDataLoading(true);
-                    // getUnitAppointmentsByCPF(selectedUnit, cpfToFilter, is7DayFilterChecked);
-                  }}
-                >
-                  Filtrar
-                </button>
-              </div>
-            </AccordionDetails>
-          </Accordion> */}
-          {/* <Accordion>
-                        <AccordionSummary>
-                            <Typography>Filtrar por data</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <div className='flex gap-2'>
-                                <DatePicker
-                                    value={new Date()}
-                                    onChange={() => { }}
-                                    renderInput={(params) => <TextField sx={{ width: '100%' }} {...params} />}
-                                />
-                                <button className='bg-green-500 py-2 px-6 rounded text-white'>Filtrar</button>
-                            </div>
-                        </AccordionDetails>
-                    </Accordion> */}
-          {/* <FormControlLabel
-            control={
-              <Checkbox
-                checked={is7DayFilterChecked}
-                onChange={event => {
-                  setIsAppointmentDataLoading(true);
-                  setIs7DayFilterChecked(event.target.checked);
-                  // getUnitAppointments(selectedUnit, event.target.checked);
-                }}
-                defaultChecked
-              />
-            }
-            label="Proximos 7 dias"
-          /> */}
-        </div>
 
         <div className="mt-8 flex justify-center w-full">
           {unitAppointments.isFetching ? (
@@ -302,68 +178,6 @@ const Dashboard = () => {
                   'Ainda não existem agendamentos para a unidade selecionada'}
               </div>
             ))
-
-            // <Paper sx={{ width: '100%' }}>
-            //   <Table>
-            //     <TableHead>
-            //       <TableRow style={{ backgroundColor: '#023E84' }}>
-            //         <TableCell style={{ color: 'white' }} align="center">
-            //           Nome
-            //         </TableCell>
-            //         <TableCell style={{ color: 'white' }} align="center">
-            //           CPF
-            //         </TableCell>
-            //         <TableCell style={{ color: 'white' }} align="center">
-            //           Atendimento
-            //         </TableCell>
-            //         <TableCell style={{ color: 'white' }} align="center">
-            //           Data
-            //         </TableCell>
-            //         <TableCell style={{ color: 'white' }} align="center">
-            //           Status
-            //         </TableCell>
-            //         <TableCell style={{ color: 'white' }} align="center">
-            //           Cancelar
-            //         </TableCell>
-            //       </TableRow>
-            //     </TableHead>
-            //     <TableBody>
-            //       {unitAppointments.data?.data.map(appointment => (
-            //         <TableRow key={appointment.id}>
-            //           <TableCell align="center">{appointment.name}</TableCell>
-            //           <TableCell align="center">
-            //             {cpfFormatter(appointment.cpf)}
-            //           </TableCell>
-            //           <TableCell align="center">
-            //             {/* {getAppointmentTypeName(
-            //               appointment.appointment_type_id,
-            //             )} */}
-            //           </TableCell>
-            //           <TableCell align="center">
-            //             {dayjs(appointment.date).format('DD/MM/YYYY')}
-            //           </TableCell>
-            //           <TableCell align="center">
-            //             {appointment.status.charAt(0).toUpperCase() +
-            //               appointment.status.slice(1)}
-            //           </TableCell>
-            //           <TableCell align="center">
-            //             <button
-            //               className="bg-red-400 py-2 px-4 rounded text-white"
-            //               color="error"
-            //               onClick={() => {
-            //                 console.log('CANCELAR: ', appointment);
-            //                 setIsAppointmentDataLoading(true);
-            //                 cancelAppointment.mutate(appointment.id);
-            //               }}
-            //             >
-            //               Cancelar
-            //             </button>
-            //           </TableCell>
-            //         </TableRow>
-            //       ))}
-            //     </TableBody>
-            //   </Table>
-            // </Paper>
           )}
         </div>
       </div>
